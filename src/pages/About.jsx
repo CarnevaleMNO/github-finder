7631b9731930891ef398595be85a0af058a47dd0
Matthew Carnevale <carnevalemno@gmail.com>
2022-01-16 18:35:22 +0900

    project template

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <div>
+            <h1 className="text-6xl mb-4">
+                Github Finder
+            </h1>
+            <p className="mb-4 text-2xl">
+                A react app featuring the Github API
+            </p>
+        </div>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+            <div className="text-6xl">
+                Welcome
+            </div>
+        </div>
+    )
+}
